8a83e4cb3aee26fd42197f7d8a808118d2c1ca32
Benedikt Spohr <bspohr@mobivention.com>
2018-03-15 17:14:30 +0100

    Added: iPhone version

--- MatomoTracker/UserAgent.swift
+++ MatomoTracker/UserAgent.swift
@@ -10,9 +10,38 @@ import UIKit
 
 struct UserAgent {
 
-    static var uaString: String? {
-        let webView = UIWebView(frame: CGRect.zero)
-        return webView.stringByEvaluatingJavaScript(from: "navigator.userAgent")
+    static func DarwinVersion() -> String {
+        var sysinfo = utsname()
+        uname(&sysinfo)
+        let dv = String(bytes: Data(bytes: &sysinfo.release, count: Int(_SYS_NAMELEN)), encoding: .ascii)!.trimmingCharacters(in: .controlCharacters)
+        return "Darwin/\(dv)"
+    }
+    static func CFNetworkVersion() -> String {
+        let dictionary = Bundle(identifier: "com.apple.CFNetwork")?.infoDictionary!
+        let version = dictionary?["CFBundleShortVersionString"] as! String
+        return "CFNetwork/\(version)"
+    }
+    
+    static func deviceVersion() -> String {
+        let currentDevice = UIDevice.current
+        return "\(currentDevice.systemName)/\(currentDevice.systemVersion)"
+    }
+    
+    static func deviceName() -> String {
+        var sysinfo = utsname()
+        uname(&sysinfo)
+        return String(bytes: Data(bytes: &sysinfo.machine, count: Int(_SYS_NAMELEN)), encoding: .ascii)!.trimmingCharacters(in: .controlCharacters)
+    }
+    
+    static func appNameAndVersion() -> String {
+        let dictionary = Bundle.main.infoDictionary!
+        let version = dictionary["CFBundleShortVersionString"] as! String
+        let name = dictionary["CFBundleName"] as! String
+        return "\(name)/\(version)"
+    }
+    
+    static var uaString: String {
+        return "\(appNameAndVersion()) \(deviceName()) \(deviceVersion()) \(CFNetworkVersion()) \(DarwinVersion())"
     }
     
     static var screenSize: String {
